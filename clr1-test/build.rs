42012d4a673c36e5d2b47a3a63c9a53546b288e2
Sharp0802 <sharp0802.business@gmail.com>
2026-01-14 18:58:34 +0900

    :recycle: Use `eprintln` instead of direct unwrap

--- clr1-test/build.rs
+++ clr1-test/build.rs
@@ -1,19 +1,31 @@
 use std::env;
 use std::fs::write;
 use std::path::Path;
+use std::process::ExitCode;
 
-fn main() {
-    let base = env!("CARGO_MANIFEST_DIR");
-    let lexer = format!("{}/../test.lex", base);
-    let parser = format!("{}/../test.parse", base);
+fn main() -> ExitCode {
+    let base = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let lexer = base.join("../test.lex").canonicalize().unwrap();
+    let parser = base.join("../test.parse").canonicalize().unwrap();
 
-    println!("cargo:rerun-if-changed={}", lexer);
-    println!("cargo:rerun-if-changed={}", parser);
+    println!("cargo:rerun-if-changed={}", lexer.display());
+    println!("cargo:rerun-if-changed={}", parser.display());
 
     let dir = env::var("OUT_DIR").expect("OUT_DIR environment variable not defined");
     let output = Path::new(&dir).join("clr1.rs");
 
-    let generated = clr1_build::build(&lexer, &parser).expect("failed to generate parser");
+    let generated = match clr1_build::build(&lexer, &parser) {
+        Ok(generated) => generated,
+        Err(e) => {
+            eprintln!("{}{}", lexer.display(), e);
+            return ExitCode::FAILURE;
+        }
+    };
 
-    write(&output, &generated).expect("failed to write generated parser");
+    if let Err(e) = write(&output, &generated) {
+        eprintln!("{}: {}", output.display(), e);
+        return ExitCode::FAILURE;
+    }
+
+    ExitCode::SUCCESS
 }
